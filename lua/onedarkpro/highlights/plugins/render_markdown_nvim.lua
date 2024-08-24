ddebe9f22439c05fe73fa67c9b6adb082c4dc450
231tr0n <zeltronsrikar@gmail.com>
2024-08-24 10:33:33 +0000

    feat(render-markdown): Add render-markdown highlight groups

--- lua/onedarkpro/highlights/plugins/render_markdown_nvim.lua
+++ lua/onedarkpro/highlights/plugins/render_markdown_nvim.lua
@@ -5,7 +5,19 @@ local M = {}
 ---@return table
 function M.groups(theme)
     return {
-        MiniIndentscopeSymbol = { fg = theme.palette.purple },
+        RenderMarkdownH1 = { fg = theme.grey, bold = true },
+        RenderMarkdownH2 = { fg = theme.grey, bold = true },
+        RenderMarkdownH3 = { fg = theme.grey, bold = true },
+        RenderMarkdownH4 = { fg = theme.grey, bold = true },
+        RenderMarkdownH5 = { fg = theme.grey, bold = true },
+        RenderMarkdownH6 = { fg = theme.grey, bold = true },
+
+        RenderMarkdownH1Bg = { bg = theme.orange },
+        RenderMarkdownH2Bg = { bg = theme.cyan },
+        RenderMarkdownH3Bg = { bg = theme.blue },
+        RenderMarkdownH4Bg = { bg = theme.purple },
+        RenderMarkdownH5Bg = { bg = theme.yellow },
+        RenderMarkdownH6Bg = { bg = theme.green },
     }
 end
 
