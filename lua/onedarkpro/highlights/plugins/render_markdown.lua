9baef00734bddb5d92db0a72f58ac2bd83b864d2
231tr0n <zeltronsrikar@gmail.com>
2024-08-24 14:30:21 +0000

    fix(render-markdown): add render-markdown to config

--- lua/onedarkpro/highlights/plugins/render_markdown.lua
+++ lua/onedarkpro/highlights/plugins/render_markdown.lua
@@ -11,12 +11,12 @@ function M.groups(theme)
         RenderMarkdownH4 = { fg = theme.palette.purple },
         RenderMarkdownH5 = { fg = theme.palette.yellow },
         RenderMarkdownH6 = { fg = theme.palette.green },
-        RenderMarkdownH1Bg = { bg = theme.palette.grey },
-        RenderMarkdownH2Bg = { bg = theme.palette.grey },
-        RenderMarkdownH3Bg = { bg = theme.palette.grey },
-        RenderMarkdownH4Bg = { bg = theme.palette.grey },
-        RenderMarkdownH5Bg = { bg = theme.palette.grey },
-        RenderMarkdownH6Bg = { bg = theme.palette.grey },
+        RenderMarkdownH1Bg = { bg = theme.palette.gray },
+        RenderMarkdownH2Bg = { bg = theme.palette.gray },
+        RenderMarkdownH3Bg = { bg = theme.palette.gray },
+        RenderMarkdownH4Bg = { bg = theme.palette.gray },
+        RenderMarkdownH5Bg = { bg = theme.palette.gray },
+        RenderMarkdownH6Bg = { bg = theme.palette.gray },
     }
 end
 
