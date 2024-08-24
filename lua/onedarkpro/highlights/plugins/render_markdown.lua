b3fcc598b093b9f5325db889772e88d295fa8d3d
231tr0n <zeltronsrikar@gmail.com>
2024-08-24 14:28:36 +0000

    fix(render-markdown): add render-markdown to config

--- lua/onedarkpro/highlights/plugins/render_markdown.lua
+++ lua/onedarkpro/highlights/plugins/render_markdown.lua
@@ -5,18 +5,18 @@ local M = {}
 ---@return table
 function M.groups(theme)
     return {
-        RenderMarkdownH1Bg = { bg = theme.palette.orange },
-        RenderMarkdownH2Bg = { bg = theme.palette.cyan },
-        RenderMarkdownH3Bg = { bg = theme.palette.blue },
-        RenderMarkdownH4Bg = { bg = theme.palette.purple },
-        RenderMarkdownH5Bg = { bg = theme.palette.yellow },
-        RenderMarkdownH6Bg = { bg = theme.palette.green },
-        RenderMarkdownH1Bg = { fg = theme.palette.grey },
-        RenderMarkdownH2Bg = { fg = theme.palette.grey },
-        RenderMarkdownH3Bg = { fg = theme.palette.grey },
-        RenderMarkdownH4Bg = { fg = theme.palette.grey },
-        RenderMarkdownH5Bg = { fg = theme.palette.grey },
-        RenderMarkdownH6Bg = { fg = theme.palette.grey },
+        RenderMarkdownH1 = { fg = theme.palette.orange },
+        RenderMarkdownH2 = { fg = theme.palette.cyan },
+        RenderMarkdownH3 = { fg = theme.palette.blue },
+        RenderMarkdownH4 = { fg = theme.palette.purple },
+        RenderMarkdownH5 = { fg = theme.palette.yellow },
+        RenderMarkdownH6 = { fg = theme.palette.green },
+        RenderMarkdownH1Bg = { bg = theme.palette.grey },
+        RenderMarkdownH2Bg = { bg = theme.palette.grey },
+        RenderMarkdownH3Bg = { bg = theme.palette.grey },
+        RenderMarkdownH4Bg = { bg = theme.palette.grey },
+        RenderMarkdownH5Bg = { bg = theme.palette.grey },
+        RenderMarkdownH6Bg = { bg = theme.palette.grey },
     }
 end
 
